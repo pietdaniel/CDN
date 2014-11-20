4ead224d86b9a94a10e52afcc2978b959057ef0a
pietdaniel <pietdaniel@gmail.com>
2014-11-19 23:35:36 -0500

    dns implemented

---
+++ dns/dnsserver.py
@@ -0,0 +1,55 @@
+import socket, sys
+
+class Query:
+    def __init__(self, data):
+        self.data = data
+        self.domain=''
+
+        # parse out domain
+        opcode = (ord(data[2]) >> 3) & 15
+        if opcode == 0:
+            cursor = 12
+            length = ord(data[cursor])
+            while length!=0:
+                self.domain+=data[cursor+1:cursor+length+1]+'.'
+                cursor+=length+1
+                length=ord(data[cursor])
+
+    def answer(self, ip):
+        packet=''
+        if self.domain:
+            packet+=self.data[:2] + "\x81\x80"
+            # q/a counts
+            packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'
+            # original dns q
+            packet+=self.data[12:len(self.data)-11]
+            # point to domain name
+            packet+='\xc0\x0c'
+            # response type, ttl, length
+            packet+='\x00\x01\x00\x01\x00\x00\x00\x4c\x00\x04'
+            # ip bytes
+            packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.')))
+        return packet
+
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(('',53))
+    except Exception, e:
+        print 'failed to create socket %s' % e
+        sys.exit(1)
+
+    print 'Running dns server'
+
+    try:
+        while 1:
+            data, addr = s.recvfrom(1024)
+            p=Query(data)
+            s.sendto(p.answer('192.168.1.5'), addr)
+            print '%s -> %s' % (p.domain, ip)
+    except KeyboardInterrupt:
+        print 'Keyboard Interrupt'
+        s.close()
+
+main()
