0839f99fd97f4af67f8cc3095d6490a2af496fdb
pietdaniel <pietdaniel@gmail.com>
2014-11-25 17:54:15 -0500

    cleaing

--- src/dns/qt.py
+++
@@ -1,4 +0,0 @@
-from dnsserver import Query
-q = Query("ASDFASDF")
-out = q.question("google.com")
-print out
--- src/http/httpserver.py
+++ src/http/httpserver.py
@@ -1,10 +1,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import urllib
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print self.path
-        print self.origin
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
