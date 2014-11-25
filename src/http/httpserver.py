9bf79c875e5c4677221a64f3b009ac4718c83317
pietdaniel <pietdaniel@gmail.com>
2014-11-25 18:22:04 -0500

    cache and variable implemented

--- src/http/httpserver.py
+++ src/http/httpserver.py
@@ -2,24 +2,35 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import urllib2
 
 
+
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        site_origin = "ec2-54-164-51-70.compute-1.amazonaws.com"
-        request = "http://" + site_origin + ":8080" + self.path
-        print request
-        response = urllib2.urlopen(request)
+        request = "http://" + self.origin + ":8080" + self.path
+
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write(response.read())
+        try:
+            response = self.cache[self.path]
+            # cache hit
+            self.wfile.write(response)
+        except KeyError as e:
+            # cache miss
+            response = urllib2.urlopen(request)
+            self.cache[self.path] = response.read()
+            self.wfile.write(response.read())
         return
 
 def run(port, origin):
     try:
-        server = HTTPServer(("",port),HttpHandler)
+        handler = HttpHandler
+        handler.origin = origin
+
+        handler.cache = {}
+
+        server = HTTPServer(("",port),handler)
         print "Started server at ", server.socket.getsockname()
         server.serve_forever()
     except KeyboardInterrupt:
         print "Keyboard Interrupt"
         server.socket.close()
-
