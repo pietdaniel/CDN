b82f1669efbf2f5c9a7f13b80562b813370aa58b
wtome <willtome@gmail.com>
2014-12-11 22:16:31 -0500

    fix http

--- src/http/httpserver.py
+++ src/http/httpserver.py
@@ -28,7 +28,7 @@ class HttpHandler(BaseHTTPRequestHandler):
             # cache miss
             print "Miss"
             try:
-                opener = urllib2.build_opener(RedirectHandler)
+                opener = urllib2.build_opener(HTTPHandler())
                 urllib2.install_opener(opener)
                 response = urllib2.urlopen(request)
                 self.make_headers(response.code)
